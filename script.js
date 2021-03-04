4c8d81c634741c1b1c5b82d35552a741ee440286
Ajakaiye Afeso <marcusitovie@gmail.com>
2021-03-04 11:10:58 +0100

    Add initial file

---
+++ script.js
@@ -0,0 +1,12 @@
+const btn = document.querySelector(".btn");
+const colorText = document.querySelector(".color-text");
+const body = document.querySelector("body");
+
+btn.addEventListener("click", function() {
+	const red = Math.floor(Math.random() * 256);
+	const green = Math.floor(Math.random() * 256);
+	const blue = Math.floor(Math.random() * 256);
+	const randomColor = `rgb(${red}, ${green}, ${blue})`;
+	body.style.backgroundColor = randomColor;
+	colorText.textContent = randomColor;
+});
